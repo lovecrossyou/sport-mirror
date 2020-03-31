c42a8fa50c5f768d0161f3f61f2ba16fe0cd54bb
lovecrossyou <lovecrossyoutt@hotmail.com>
2020-03-31 23:30:15 +0800

    profile

--- src/pages/me/profile.js
+++ src/pages/me/profile.js
@@ -5,33 +5,40 @@ import {
   StyleSheet,
   FlatList,
   SafeAreaView,
-  TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import { ProfileItem, ProfileHeader } from './components/profile-item';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#f5f5f5'
+    backgroundColor: '#f5f5f5'
   }
 })
 
 export default () => {
-  return <View style={styles.container}>
-    <ProfileHeader />
-    <ProfileItem title="Email" desc="Kevin.zhang@wizsolu.com" />
-    <ProfileItem title="City" desc="721 Moorestown NJ" />
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <ProfileHeader />
+        <ProfileItem title="Email" desc="Kevin.zhang@wizsolu.com" />
+        <ProfileItem title="City" desc="721 Moorestown NJ" />
 
-    <ProfileItem title="Age" desc="42" />
+        <ProfileItem title="Age" desc="42" />
 
-    <ProfileItem title="Gender" desc="Male" />
+        <ProfileItem title="Gender" desc="Male" />
 
-    <ProfileItem title="Height" desc="5‘7’‘" />
+        <ProfileItem title="Height" desc="5‘7’‘" />
 
-    <ProfileItem title="Weight" desc="177" />
+        <ProfileItem title="Weight" desc="170 lbs" />
 
-    <ProfileItem title="Max Heart Rate" desc="Kevin.zhang@wizsolu.com" />
-    <ProfileItem title="Ability" desc="Barre,Cardio, Dance,+3" />
+        <ProfileItem title="Max Heart Rate" desc="177" />
+        <ProfileItem title="Ability" desc="Barre,Cardio, Dance,+3" />
+        <ProfileItem title="Goal" desc="Build Muscle" />
+        <ProfileItem title="Liimitations" desc="" />
 
-  </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+
 }
