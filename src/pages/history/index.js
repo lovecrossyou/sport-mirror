e765637af9c4fef42e12976502bdd4f1f72cef9f
lovecrossyou <lovecrossyoutt@hotmail.com>
2020-04-01 21:56:49 +0800

    async code

--- src/pages/history/index.js
+++ src/pages/history/index.js
@@ -12,9 +12,19 @@ import Tiltle from '@/pages/components/title';
 import WeekPanel from './components/week';
 import Summary from './components/summary';
 import Daily from './components/daily';
+import {setSpText} from '@/util/screen';
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.header_title}>HISTORY</Text>
+    </View>
+  );
+};
 export default () => {
   return (
     <SafeAreaView style={styles.container}>
+      <Header/>
       <Tiltle name="WEEKLY GOAL"></Tiltle>
       <WeekPanel />
 
@@ -34,5 +44,19 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     backgroundColor: '#fff',
+  },
+  header: {
+    width: '100%',
+    height: 44,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 2,
+    borderColor: '#EEEEEE',
+    backgroundColor: '#FFFFFF',
+  },
+  header_title: {
+    fontSize: setSpText(32),
+    color: '#000000',
+    fontWeight: '600',
   },
 });
