acbc60c1839abd76b95487826193c6478766c4db
lovecrossyou <lovecrossyoutt@hotmail.com>
2020-04-06 20:16:12 +0800

    aslint config

--- src/pages/history/index.js
+++ src/pages/history/index.js
@@ -1,18 +1,11 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  FlatList,
-  SafeAreaView,
-  TouchableOpacity,
-  ScrollView
-} from 'react-native';
+import {Text, View, StyleSheet, SafeAreaView, ScrollView} from 'react-native';
 import Tiltle from '@/pages/components/title';
 import WeekPanel from './components/week';
 import Summary from './components/summary';
 import Daily from './components/daily';
-import { setSpText } from '@/util/screen';
+import {setSpText} from '@/util/screen';
 
 const Header = () => {
   return (
@@ -25,19 +18,19 @@ export default () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
-      <Tiltle name="WEEKLY GOAL"></Tiltle>
+      <Tiltle name="WEEKLY GOAL" />
       <WeekPanel />
 
-      <ScrollView alwaysBounceHorizontal={false} style={{ width: '100%' }}>
-        <Tiltle name="WEEKLY SUMMARY"></Tiltle>
+      <ScrollView alwaysBounceHorizontal={false} style={{width: '100%'}}>
+        <Tiltle name="WEEKLY SUMMARY" />
         <Summary />
 
-        <Tiltle name="DAILY SUMMARY"></Tiltle>
+        <Tiltle name="DAILY SUMMARY" />
         <Daily />
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
--- src/pages/me/components/profile-item.js
+++ src/pages/me/components/profile-item.js
@@ -1,81 +1,77 @@
 import React from 'react';
-import {
-    Text,
-    View,
-    StyleSheet,
-    Image,
-    SafeAreaView,
-    TouchableOpacity,
-} from 'react-native';
-import { scaleSizeW, scaleSizeH, setSpText } from '@/util/screen';
+import {Text, View, StyleSheet, Image} from 'react-native';
+import {scaleSizeW, scaleSizeH, setSpText} from '@/util/screen';
 import icon_me_arrow from '@/assets/me/icon_me_arrow.png';
 
 const styles = StyleSheet.create({
-    header: {
-        height: scaleSizeH(160),
-        width: '100%',
-        backgroundColor: '#fff',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingLeft:scaleSizeW(38),
-        paddingRight:scaleSizeW(38),
-        marginTop: scaleSizeH(30),
-        marginBottom: scaleSizeH(30)
-    },
-    header_s: {
-        height: scaleSizeH(120),
-        width: '100%',
-        backgroundColor: '#fff',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingLeft:scaleSizeW(38),
-        paddingRight:scaleSizeW(38),
-        borderBottomColor:'#EEEEEE',
-        borderBottomWidth:scaleSizeH(1)
-    },
-    avatar: {
-        fontSize: setSpText(32),
-        color: '#131313'
-    },
-    header_right: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        
-    },
-    arrow: {
-        width: scaleSizeW(15),
-        height: scaleSizeH(27),
-        marginLeft: scaleSizeW(25)
-    },
-    avatar_icon: {
-        width: scaleSizeW(120),
-        height: scaleSizeH(120),
-        borderRadius: scaleSizeH(60)
-    },
-    desc: {
-        fontSize: setSpText(32),
-        color: '#777777'
-    }
-})
+  header: {
+    height: scaleSizeH(160),
+    width: '100%',
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingLeft: scaleSizeW(38),
+    paddingRight: scaleSizeW(38),
+    marginTop: scaleSizeH(30),
+    marginBottom: scaleSizeH(30),
+  },
+  header_s: {
+    height: scaleSizeH(120),
+    width: '100%',
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingLeft: scaleSizeW(38),
+    paddingRight: scaleSizeW(38),
+    borderBottomColor: '#EEEEEE',
+    borderBottomWidth: scaleSizeH(1),
+  },
+  avatar: {
+    fontSize: setSpText(32),
+    color: '#131313',
+  },
+  header_right: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  arrow: {
+    width: scaleSizeW(15),
+    height: scaleSizeH(27),
+    marginLeft: scaleSizeW(25),
+  },
+  avatar_icon: {
+    width: scaleSizeW(120),
+    height: scaleSizeH(120),
+    borderRadius: scaleSizeH(60),
+  },
+  desc: {
+    fontSize: setSpText(32),
+    color: '#777777',
+  },
+});
 
 export const ProfileHeader = () => {
-    return <View style={styles.header}>
-        <Text style={styles.avatar}>Avatar</Text>
-        <View style={styles.header_right}>
-            <Image style={styles.avatar_icon} />
-            <Image source={icon_me_arrow} style={styles.arrow} />
-        </View>
+  return (
+    <View style={styles.header}>
+      <Text style={styles.avatar}>Avatar</Text>
+      <View style={styles.header_right}>
+        <Image style={styles.avatar_icon} />
+        <Image source={icon_me_arrow} style={styles.arrow} />
+      </View>
     </View>
-}
+  );
+};
 
-export const ProfileItem = ({ title, desc }) => {
-    return <View style={styles.header_s}>
-        <Text style={styles.avatar}>{title}</Text>
-        <View style={styles.header_right}>
-            <Text style={styles.desc}>{desc}</Text>
-            <Image source={icon_me_arrow} style={styles.arrow} />
-        </View>
+export const ProfileItem = ({title, desc}) => {
+  return (
+    <View style={styles.header_s}>
+      <Text style={styles.avatar}>{title}</Text>
+      <View style={styles.header_right}>
+        <Text style={styles.desc}>{desc}</Text>
+        <Image source={icon_me_arrow} style={styles.arrow} />
+      </View>
     </View>
-}
+  );
+};
