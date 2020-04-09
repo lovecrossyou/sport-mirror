e74d1dd85d0b49bda4949d63e5256945d5dceb32
tianhaojie <1689177720@qq.com>
2020-04-09 21:53:28 +0800

    蓝牙连接无效弹框modal

--- src/pages/me/components/me-modal.js
+++ src/pages/me/components/me-modal.js
@@ -10,6 +10,8 @@ import {
 } from 'react-native';
 import Modal from 'react-native-modal';
 import {scaleSizeW, scaleSizeH, setSpText} from '@/util/screen';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import icon_me_close from '@/assets/me/icon_me_close.png';
 export class BluetoothUnavailableModal extends Component {
   state = {
     isModalVisible: false,
@@ -28,9 +30,30 @@ export class BluetoothUnavailableModal extends Component {
   render() {
     const deviceWidth = Dimensions.get('window').width;
     return (
-      <Modal isVisible={this.state.isModalVisible} deviceWidth={deviceWidth}>
+      <Modal
+        style={{margin: 0}}
+        isVisible={this.state.isModalVisible}
+        deviceWidth={deviceWidth}
+        swipeDirection="left">
         <View style={styles.modal_container}>
-          <View style={styles.bluetooth_unavailable_container} />
+          <View style={styles.bluetooth_unavailable_container}>
+            <View style={styles.bluetooth_unavailable_title}>
+              <View />
+              <Text style={styles.bluetooth_unavailable_title_text}>
+                HEART RATE MONITOR
+              </Text>
+
+              <TouchableWithoutFeedback onPress={() => this.closeModal()}>
+                <Image source={icon_me_close} style={styles.close_btn} />
+              </TouchableWithoutFeedback>
+            </View>
+            <Text style={styles.bluetooth_unavailable_content_text}>
+              Bluetooth Unavailable
+            </Text>
+            <Text style={styles.bluetooth_unavailable_sub_text}>
+              Please make sure Bluetooth is enabled in your phone’s settings.
+            </Text>
+          </View>
         </View>
       </Modal>
     );
@@ -44,10 +67,47 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
   },
   bluetooth_unavailable_container: {
+    borderTopLeftRadius: scaleSizeW(33),
+    borderTopRightRadius: scaleSizeW(33),
     backgroundColor: '#ffffff',
     width: '100%',
-    height: 300,
+    paddingBottom: scaleSizeH(289),
     flexDirection: 'column',
     alignItems: 'flex-end',
+  },
+  bluetooth_unavailable_title: {
+    width: '100%',
+    height: scaleSizeH(120),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: scaleSizeW(45),
+    borderBottomWidth: scaleSizeH(1),
+    borderBottomColor: '#EEEEEE',
+  },
+  bluetooth_unavailable_title_text: {
+    fontSize: setSpText(32),
+    color: '#131313',
+    fontWeight: '500',
+  },
+  close_btn: {
+    width: scaleSizeW(33),
+    height: scaleSizeW(33),
+  },
+  bluetooth_unavailable_content_text: {
+    width: '100%',
+    marginTop: scaleSizeH(170),
+    fontWeight: '500',
+    fontSize: setSpText(48),
+    color: '#131313',
+    textAlign: 'center',
+  },
+  bluetooth_unavailable_sub_text: {
+    marginHorizontal: scaleSizeW(70),
+    marginTop: scaleSizeH(40),
+    fontWeight: '400',
+    fontSize: setSpText(32),
+    color: '#999999',
+    textAlign: 'center',
   },
 });
--- src/pages/me/index.js
+++ src/pages/me/index.js
@@ -1,20 +1,5 @@
 import React, {Component} from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  SafeAreaView,
-  ScrollView,
-  TouchableHighlight,
-  NativeEventEmitter,
-  NativeModules,
-  Platform,
-  PermissionsAndroid,
-  AppState,
-  FlatList,
-  Dimensions,
-  Button,
-} from 'react-native';
+import {Text, View, StyleSheet, SafeAreaView, ScrollView} from 'react-native';
 import {setSpText} from '@/util/screen';
 import MeItemHeader from './components/me-item-header';
 import {
@@ -25,15 +10,13 @@ import {
   MeConnectMirrorView,
   MeItemNormalOnlyValue,
 } from './components/me-item-normal';
+import {BluetoothUnavailableModal} from './components/me-modal';
 import icon_me_bluetooth from '@/assets/me/icon_me_bluetooth.png';
 import icon_me_heart_rate from '@/assets/me/icon_me_heart_rate.png';
 import icon_me_household_members from '@/assets/me/icon_me_household-members.png';
 import icon_me_mirror from '@/assets/me/icon_me_mirror.png';
 import icon_me_reset from '@/assets/me/icon_me_reset.png';
 import icon_me_support from '@/assets/me/icon_me_support.png';
-import BleManager from 'react-native-ble-manager';
-const BleManagerModule = NativeModules.BleManager;
-const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 const Header = () => {
   return (
     <View style={styles.header}>
@@ -42,239 +25,6 @@ const Header = () => {
   );
 };
 export default class Me extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      scanning: false,
-      peripherals: new Map(),
-      appState: '',
-    };
-
-    this.handleDiscoverPeripheral = this.handleDiscoverPeripheral.bind(this);
-    this.handleStopScan = this.handleStopScan.bind(this);
-    this.handleUpdateValueForCharacteristic = this.handleUpdateValueForCharacteristic.bind(
-      this,
-    );
-    this.handleDisconnectedPeripheral = this.handleDisconnectedPeripheral.bind(
-      this,
-    );
-    this.handleAppStateChange = this.handleAppStateChange.bind(this);
-  }
-  componentDidMount() {
-    AppState.addEventListener('change', this.handleAppStateChange);
-
-    BleManager.start({showAlert: true});
-
-    this.handlerDiscover = bleManagerEmitter.addListener(
-      'BleManagerDiscoverPeripheral',
-      this.handleDiscoverPeripheral,
-    );
-    this.handlerStop = bleManagerEmitter.addListener(
-      'BleManagerStopScan',
-      this.handleStopScan,
-    );
-    this.handlerDisconnect = bleManagerEmitter.addListener(
-      'BleManagerDisconnectPeripheral',
-      this.handleDisconnectedPeripheral,
-    );
-    this.handlerUpdate = bleManagerEmitter.addListener(
-      'BleManagerDidUpdateValueForCharacteristic',
-      this.handleUpdateValueForCharacteristic,
-    );
-
-    if (Platform.OS === 'android' && Platform.Version >= 23) {
-      PermissionsAndroid.check(
-        PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
-      ).then(result => {
-        if (result) {
-          console.log('Permission is OK');
-        } else {
-          PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
-          ).then(result => {
-            if (result) {
-              console.log('User accept');
-            } else {
-              console.log('User refuse');
-            }
-          });
-        }
-      });
-    }
-  }
-
-  handleAppStateChange(nextAppState) {
-    if (
-      this.state.appState.match(/inactive|background/) &&
-      nextAppState === 'active'
-    ) {
-      console.log('App has come to the foreground!');
-      BleManager.getConnectedPeripherals([]).then(peripheralsArray => {
-        console.log('Connected peripherals: ' + peripheralsArray.length);
-      });
-    }
-    this.setState({appState: nextAppState});
-  }
-
-  componentWillUnmount() {
-    this.handlerDiscover.remove();
-    this.handlerStop.remove();
-    this.handlerDisconnect.remove();
-    this.handlerUpdate.remove();
-  }
-
-  handleDisconnectedPeripheral(data) {
-    let peripherals = this.state.peripherals;
-    let peripheral = peripherals.get(data.peripheral);
-    if (peripheral) {
-      peripheral.connected = false;
-      peripherals.set(peripheral.id, peripheral);
-      this.setState({peripherals});
-    }
-    console.log('Disconnected from ' + data.peripheral);
-  }
-
-  handleUpdateValueForCharacteristic(data) {
-    console.log(
-      'Received data from ' +
-        data.peripheral +
-        ' characteristic ' +
-        data.characteristic,
-      data.value,
-    );
-  }
-
-  handleStopScan() {
-    console.log('Scan is stopped');
-    this.setState({scanning: false});
-  }
-
-  startScan() {
-    if (!this.state.scanning) {
-      //this.setState({peripherals: new Map()});
-      BleManager.scan([], 3, true).then(results => {
-        console.log('Scanning...');
-        this.setState({scanning: true});
-      });
-    }
-  }
-
-  retrieveConnected() {
-    BleManager.getConnectedPeripherals([]).then(results => {
-      if (results.length == 0) {
-        console.log('No connected peripherals');
-      }
-      console.log(results);
-      var peripherals = this.state.peripherals;
-      for (var i = 0; i < results.length; i++) {
-        var peripheral = results[i];
-        peripheral.connected = true;
-        peripherals.set(peripheral.id, peripheral);
-        this.setState({peripherals});
-      }
-    });
-  }
-
-  handleDiscoverPeripheral(peripheral) {
-    var peripherals = this.state.peripherals;
-    console.log('Got ble peripheral', peripheral);
-    if (!peripheral.name) {
-      peripheral.name = 'NO NAME';
-    }
-    peripherals.set(peripheral.id, peripheral);
-    this.setState({peripherals});
-  }
-
-  test(peripheral) {
-    if (peripheral) {
-      if (peripheral.connected) {
-        BleManager.disconnect(peripheral.id);
-      } else {
-        BleManager.connect(peripheral.id)
-          .then(() => {
-            let peripherals = this.state.peripherals;
-            let p = peripherals.get(peripheral.id);
-            if (p) {
-              p.connected = true;
-              peripherals.set(peripheral.id, p);
-              this.setState({peripherals});
-            }
-            console.log('Connected to ' + peripheral.id);
-
-            setTimeout(() => {
-              /* Test read current RSSI value
-            BleManager.retrieveServices(peripheral.id).then((peripheralData) => {
-              console.log('Retrieved peripheral services', peripheralData);
-
-              BleManager.readRSSI(peripheral.id).then((rssi) => {
-                console.log('Retrieved actual RSSI value', rssi);
-              });
-            });*/
-
-              // Test using bleno's pizza example
-              // https://github.com/sandeepmistry/bleno/tree/master/examples/pizza
-              BleManager.retrieveServices(peripheral.id).then(
-                peripheralInfo => {
-                  console.log(peripheralInfo);
-                  var service = '13333333-3333-3333-3333-333333333337';
-                  var bakeCharacteristic =
-                    '13333333-3333-3333-3333-333333330003';
-                  var crustCharacteristic =
-                    '13333333-3333-3333-3333-333333330001';
-
-                  setTimeout(() => {
-                    BleManager.startNotification(
-                      peripheral.id,
-                      service,
-                      bakeCharacteristic,
-                    )
-                      .then(() => {
-                        console.log('Started notification on ' + peripheral.id);
-                        setTimeout(() => {
-                          BleManager.write(
-                            peripheral.id,
-                            service,
-                            crustCharacteristic,
-                            [0],
-                          ).then(() => {
-                            console.log('Writed NORMAL crust');
-                            BleManager.write(
-                              peripheral.id,
-                              service,
-                              bakeCharacteristic,
-                              [1, 95],
-                            ).then(() => {
-                              console.log(
-                                'Writed 351 temperature, the pizza should be BAKED',
-                              );
-                              /*
-                        var PizzaBakeResult = {
-                          HALF_BAKED: 0,
-                          BAKED:      1,
-                          CRISPY:     2,
-                          BURNT:      3,
-                          ON_FIRE:    4
-                        };*/
-                            });
-                          });
-                        }, 500);
-                      })
-                      .catch(error => {
-                        console.log('Notification error', error);
-                      });
-                  }, 200);
-                },
-              );
-            }, 900);
-          })
-          .catch(error => {
-            console.log('Connection error', error);
-          });
-      }
-    }
-  }
-
   render() {
     return (
       <SafeAreaView style={styles.container}>
@@ -290,7 +40,7 @@ export default class Me extends Component {
             des="You’ll need to be the same WiFi network as the mirror  you want to connect to."
             itemCallBack={() => {
               console.log('CONNECT TO MIRROR');
-              this.startScan();
+              this.refs.bluetoothUnavailableModal.openModal();
             }}
           />
           <MeItemSpace />
@@ -340,6 +90,7 @@ export default class Me extends Component {
             }}
           />
           <View style={styles.scroll_bottom_space} />
+          <BluetoothUnavailableModal ref={"bluetoothUnavailableModal"} />
         </ScrollView>
       </SafeAreaView>
     );
